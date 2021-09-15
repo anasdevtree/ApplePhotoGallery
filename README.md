287cc22bba320d941a3037dfc5be913043982d46
Anas Iqbal <anasiqbal1818@gmail.com>
2021-09-15 14:57:14 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Apple-Photogallery
+ This photo gallery allow to upload more photos and it reload automatically then arranges photos itself, additionally you can click on any photo to enlarge it.
---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Apple PhotoGallery</h1>
+      <h2>Interactive React Photo Gallery</h2>
+      <p>This photo gallery allow to upload more photos and it reload automatically then arranges photos itself, <br />additionally you can click on any photo to enlarge it.</p>
+    </div>
+  )
+}
+
+export default Title;
